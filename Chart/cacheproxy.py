eff7e7aea66a6226e2b634dec2163b24334289ff
赵成钢 <2017011362@secoder.net>
2018-11-02 09:27:40 +0800

    Add the noHT points

--- Chart/cacheproxy.py
+++ Chart/cacheproxy.py
@@ -8,16 +8,16 @@ plt.xticks(x)
 # Baseline
 y = [0.1, 0.2, 0.3, 0.5]
 plt.plot(x, y, linewidth = 1, marker = '2', label = 'BL L2', ms = 4) # L2
-y = [0.05, 0.05, 0.04, 0.06]
-plt.plot(x, y, '--', linewidth = 1, marker = 's', label = 'BL L3', ms = 4) # L3
-
+y = [-1, -1, -1, 0.3] # the last number if for noHT
+plt.plot(x, y, linewidth = 0, marker = 'o', label = 'BL/noHT L2', ms = 8)
 
 # Shaking Corals
 y = [0.04, 0.1, 0.12, 0.2]
 plt.plot(x, y, linewidth = 1, marker = 'p', label = 'SC L2', ms = 4) # L2
-y = [0.07, 0.05, 0.09, 0.1]
-plt.plot(x, y, '--', linewidth = 1, marker = 'h', label = 'SC L3', ms = 4) # L3
+y = [-1, -1, -1, 0.1] # the last number if for noHT
+plt.plot(x, y, linewidth = 0, marker = '*', label = 'SC/noHT L2', ms = 8)
 
+plt.ylim(0, 1.0)
 plt.xlabel('Order')
 plt.ylabel('Miss ratio')
 plt.legend()
